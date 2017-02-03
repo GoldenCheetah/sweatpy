f55cd4b348da785c61a28fd62b006134a9208cba
Aart Goossens <aart@goossens.me>
2017-02-03 16:48:17 +0100

    FIX - Increases coverage to 100%

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,6 @@
 import unittest
 
+import numpy as np
 from pandas import DataFrame
 
 from athletic_pandas.models import WorkoutDataFrame
@@ -14,7 +15,17 @@ class TestWorkoutDataFrame(unittest.TestCase):
         self.assertIsInstance(self.wdf, DataFrame)
         self.assertIsInstance(self.wdf, WorkoutDataFrame)
 
+    def test_constructor(self):
+        self.wdf['power'] = np.nan
+        new_wdf = self.wdf.ix[:,['power']]
+        self.assertIsInstance(new_wdf, DataFrame)
+        self.assertIsInstance(new_wdf, WorkoutDataFrame)
+
     def test_mean_max_power(self):
+        self.wdf['power'] = np.nan
+        self.assertIsNone(self.wdf.mean_max_power())
+
+    def test_mean_max_power_missing_data(self):
         with self.assertRaises(MissingDataException):
             self.assertIsNone(self.wdf.mean_max_power())
 
