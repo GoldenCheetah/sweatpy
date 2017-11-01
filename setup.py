c13cfbf1989106cc9a2da799c2a13632407fa119
Aart Goossens <aart@goossens.me>
2017-11-01 20:28:00 +0100

    CHANGE - Updates version to 0.4.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'athletic_pandas',
     packages = ['athletic_pandas'],
-    version = '0.3',
+    version = '0.4.0',
     description = 'Workout analysis',
     author='Aart Goossens',
     author_email='aart@goossens.me',
