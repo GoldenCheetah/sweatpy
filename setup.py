7ebc7332f79f450c02e3087f6909e6d0142b0930
Aart Goossens <aart@goossens.me>
2017-11-01 20:26:03 +0100

    NEW - Adds setup.py and MANIFEST

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name = 'athletic_pandas',
+    packages = ['athletic_pandas'],
+    version = '0.3',
+    description = 'Workout analysis',
+    author='Aart Goossens',
+    author_email='aart@goossens.me',
+    url='https://github.com/AartGoossens/athletic_pandas',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+    ]
+)
