4e2c6e7e674c2ea0f33aa38e7375224d14b5bef4
Aart Goossens <aart@goossens.me>
2017-12-01 20:30:59 +0100

    Prepending compute_ to algorithms and fixing pytest fixtures

--- athletic_pandas/models.py
+++ athletic_pandas/models.py
@@ -7,19 +7,19 @@ class WorkoutDataFrame(BaseWorkoutDataFrame):
     _metadata = ['athlete']
 
     @requires(columns=['power'])
-    def mean_max_power(self):
+    def compute_mean_max_power(self):
         return algorithms.mean_max_power(self.power)
 
     @requires(columns=['power'])
-    def weighted_average_power(self):
+    def compute_weighted_average_power(self):
         return algorithms.weighted_average_power(self.power)
 
     @requires(columns=['power'], athlete=['weight'])
-    def power_per_kg(self):
+    def compute_power_per_kg(self):
         return algorithms.power_per_kg(self.power, self.athlete.weight)
 
     @requires(columns=['power'], athlete=['cp', 'w_prime'])
-    def w_prime_balance(self, algorithm=None, *args, **kwargs):
+    def compute_w_prime_balance(self, algorithm=None, *args, **kwargs):
         return algorithms.w_prime_balance(self.power, self.athlete.cp, 
             self.athlete.w_prime, algorithm, *args, **kwargs)
 
--- tests/test_models.py
+++ tests/test_models.py
@@ -9,12 +9,38 @@ from athletic_pandas import algorithms, exceptions, models
 
 @pytest.fixture
 def wdf():
-    athlete = models.Athlete(cp=200, w_prime=20000)
-    wdf = models.WorkoutDataFrame(
-        pd.read_csv('tests/example_files/workout_1_short.csv'))
+    data = {
+        'time': range(10),
+        'heartrate': range(10),
+        'power': range(10)}
+    athlete = models.Athlete(name='Chris', weight=80, ftp=300)
+    wdf = models.WorkoutDataFrame(data)
     wdf = wdf.set_index('time')
     wdf.athlete = athlete
     return wdf
+
+
+@pytest.fixture
+def wdf_small():
+    athlete = models.Athlete(cp=200, w_prime=20000)
+    wdf = models.WorkoutDataFrame(
+        pd.read_csv('tests/example_files/workout_1_short.csv')
+    )
+    wdf = wdf.set_index('time')
+    wdf.athlete = athlete
+    return wdf
+
+
+@pytest.fixture
+def wdf_big():
+    athlete = models.Athlete(cp=200, w_prime=20000, weight=80)
+    wdf = models.WorkoutDataFrame(
+        pd.read_csv('tests/example_files/workout_1.csv')
+    )
+    wdf = wdf.set_index('time')
+    wdf.athlete = athlete
+    return wdf
+
 
 class TestDataPoint:
     def test_init(self):
@@ -53,53 +79,36 @@ class TestAthlete:
 
 
 class TestWorkoutDataFrame:
-    def setup(self):
-        data = {
-            'time': range(10),
-            'heartrate': range(10),
-            'power': range(10)}
-        athlete = models.Athlete(name='Chris', weight=80, ftp=300)
-        self.wdf = models.WorkoutDataFrame(data)
-        self.wdf = self.wdf.set_index('time')
-        self.wdf.athlete = athlete
-
-    def _import_csv_as_wdf(self, filename='workout_1.csv'):
-        athlete = self.wdf.athlete
-        self.wdf = models.WorkoutDataFrame(
-            pd.read_csv('tests/example_files/{}'.format(filename)))
-        self.wdf = self.wdf.set_index('time')
-        self.wdf.athlete = athlete
-
     def test_empty_init(self):
         wdf = models.WorkoutDataFrame()
 
         assert isinstance(wdf, pd.DataFrame)
         assert isinstance(wdf, models.WorkoutDataFrame)
 
-    def test_init(self):
-        assert 'power' in list(self.wdf)
-        assert 'heartrate' in list(self.wdf)
-        assert len(self.wdf) == 10
-        assert hasattr(self.wdf, 'athlete')
+    def test_init(self, wdf):
+        assert 'power' in list(wdf)
+        assert 'heartrate' in list(wdf)
+        assert len(wdf) == 10
+        assert hasattr(wdf, 'athlete')
 
-    def test_slicing(self):
-        new_wdf = self.wdf[1:5]
+    def test_slicing(self, wdf):
+        new_wdf = wdf[1:5]
 
         assert isinstance(new_wdf, models.WorkoutDataFrame)
 
-    def test_metadata_propagation(self):
-        assert self.wdf[1:5].athlete.name == 'Chris'
-        assert self.wdf.iloc[[0, 1], :].athlete.name == 'Chris'
-        assert self.wdf[['power']].athlete.name == 'Chris'
+    def test_metadata_propagation(self, wdf):
+        assert wdf[1:5].athlete.name == 'Chris'
+        assert wdf.iloc[[0, 1], :].athlete.name == 'Chris'
+        assert wdf[['power']].athlete.name == 'Chris'
 
-    def test_is_valid(self):
-        assert self.wdf.is_valid()
+    def test_is_valid(self, wdf):
+        assert wdf.is_valid()
 
-    def test_is_valid_missing_time_index(self):
-        self.wdf.index.name = 'not_time'
+    def test_is_valid_missing_time_index(self, wdf):
+        wdf.index.name = 'not_time'
 
         with pytest.raises(exceptions.WorkoutDataFrameValidationException):
-            self.wdf.is_valid()
+            wdf.is_valid()
 
     def test_is_valid_invalid_sample_rate(self):
         data = {
@@ -149,46 +158,39 @@ class TestWorkoutDataFrame:
             wdf.is_valid()
             assert e.message == '[.\n]*Column \'power\' has values > 3000[.\n]*'
 
-    def test_mean_max_power(self):
-        self._import_csv_as_wdf()
-        mmp = self.wdf.mean_max_power()
+    def test_compute_mean_max_power(self, wdf_big):
+        mmp = wdf_big.compute_mean_max_power()
 
         assert mmp.iloc[1] == 263.0
         assert mmp.iloc[300] == 209.37209302325581
 
-    def test_mean_max_power_missing_power(self):
-        del self.wdf['power']
+    def test_compute_mean_max_power_missing_power(self, wdf):
+        del wdf['power']
 
         with pytest.raises(exceptions.MissingDataException):
-            assert self.wdf.mean_max_power() is None
+            assert wdf.compute_mean_max_power() is None
 
-    def test_weighted_average_power(self):
-        self._import_csv_as_wdf()
+    def test_compute_weighted_average_power(self, wdf_big):
+        assert wdf_big.compute_weighted_average_power() == 156.24624656343036
 
-        assert self.wdf.weighted_average_power() == 156.24624656343036
-
-    def test_weighted_average_power_missing_weight(self):
-        self._import_csv_as_wdf()
-        self.wdf.athlete.weight = None
-
-        assert self.wdf.weighted_average_power() == 156.24624656343036
+    def test_compute_weighted_average_power_missing_power(self, wdf_big):
+        del wdf_big['power']
 
         with pytest.raises(exceptions.MissingDataException):
-            self.wdf.power_per_kg()
+            wdf_big.compute_weighted_average_power()
 
-    def test_power_per_kg(self):
-        self._import_csv_as_wdf()
-        self.wdf.athlete.ftp = 300
-        ppkg = self.wdf.power_per_kg()
+    def test_compute_power_per_kg(self, wdf_big):
+        # wdf_big.athlete.ftp = 300
+        ppkg = wdf_big.compute_power_per_kg()
 
         assert ppkg[1] == 1.1625000000000001
         assert ppkg[100] == 1.0125
 
-    def test_power_per_kg_missing_weight(self):
-        self.wdf.athlete.weight = None
+    def test_compute_power_per_kg_missing_weight(self, wdf):
+        wdf.athlete.weight = None
 
         with pytest.raises(exceptions.MissingDataException):
-            self.wdf.power_per_kg()
+            wdf.compute_power_per_kg()
 
     @pytest.mark.parametrize("test_input,expected", [
         (dict(), 19174.872458854417),
@@ -200,14 +202,13 @@ class TestWorkoutDataFrame:
         (dict(algorithm='skiba', tau_dynamic=True), 19166.47851826546),
         (dict(algorithm='froncioni'), 19189.746089851626),
     ])
-    def test_w_prime_balance(self, wdf, test_input, expected):
-        w_balance = wdf.w_prime_balance(**test_input)
+    def test_compute_w_prime_balance(self, wdf_small, test_input, expected):
+        w_balance = wdf_small.compute_w_prime_balance(**test_input)
 
         assert w_balance.iloc[800] == expected
 
-    def test_compute_mean_max_bests(self):
-        self._import_csv_as_wdf()
-        result = self.wdf.compute_mean_max_bests(60, 3)
+    def test_compute_mean_max_bests(self, wdf_big):
+        result = wdf_big.compute_mean_max_bests(60, 3)
 
         assert len(result) == 3
         assert isinstance(result[0], algorithms.DataPoint)
@@ -215,17 +216,15 @@ class TestWorkoutDataFrame:
         assert result[1] == (2236, 210.48333333333332)
         assert result[2] == (2159, 208.93333333333334)
 
-    def test_compute_mean_max_bests_only_one_result(self):
-        self._import_csv_as_wdf()
-        result = self.wdf.compute_mean_max_bests(3000, 2)
+    def test_compute_mean_max_bests_only_one_result(self, wdf_big):
+        result = wdf_big.compute_mean_max_bests(3000, 2)
 
         assert len(result) == 2
         assert not result[0] == (np.nan, np.nan)
         assert result[1] == (np.nan, np.nan)
 
-    def test_compute_mean_max_bests_no_results(self):
-        self._import_csv_as_wdf()
-        result = self.wdf.compute_mean_max_bests(10000, 1)
+    def test_compute_mean_max_bests_no_results(self, wdf_big):
+        result = wdf_big.compute_mean_max_bests(10000, 1)
 
         assert len(result) == 1
         assert result[0] == (np.nan, np.nan)
