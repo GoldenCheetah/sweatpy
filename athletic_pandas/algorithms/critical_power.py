32e6ea2b712cfdde06f5882e74f52a9115a7e803
Aart Goossens <aart@goossens.me>
2018-02-24 12:26:51 +0100

    Move from lmfit to scipy curve_fit

--- athletic_pandas/algorithms/critical_power.py
+++ athletic_pandas/algorithms/critical_power.py
@@ -1,8 +1,9 @@
 import numpy as np
-from lmfit import Parameters, minimize
+from scipy.optimize import curve_fit
 
 
-def _ecp_predict_5_3(model_params, x):
+def extended_5_3_predict(x, power_anaerobic_alactic, power_anaerobic_decay,
+                     cp, tau_delay, cp_delay, cp_decay, cp_decay_delay, tau):
     """
     Credits to Damien Grauser. Source:
     https://github.com/GoldenCheetah/GoldenCheetah/blob/master/src/Metrics/ExtendedCriticalPower.cpp
@@ -10,23 +11,23 @@ def _ecp_predict_5_3(model_params, x):
     model = (
         (
             (
-                model_params['power_anaerobic_alactic']
+                power_anaerobic_alactic
             ) * (
                 1.20 - 0.20 * np.exp(-1 * x)
             ) * (
-                np.exp(model_params['power_anaerobic_decay'] * x)
+                np.exp(power_anaerobic_decay * x)
             )
         ) + (
             (
-                model_params['cp']
+                cp
             ) * (
-                1 - np.exp(model_params['cp_delay'] * x)
+                1 - np.exp(cp_delay * x)
             ) * (
-                1 + model_params['cp_decay'] * np.exp(model_params['cp_decay_delay'] / x)
+                1 + cp_decay * np.exp(cp_decay_delay / x)
             ) * (
-                1 - np.exp(model_params['tau_delay'] * x)
+                1 - np.exp(tau_delay * x)
             ) * (
-                1 + model_params['tau'] / x
+                1 + tau / x
             )
         )
     )
@@ -38,7 +39,8 @@ def _ecp_predict_5_3(model_params, x):
     return model
 
 
-def _ecp_predict_7_3(model_params, x):
+def extended_7_3_predict(x, power_anaerobic_alactic, power_anaerobic_decay,
+                     cp, tau_delay, cp_delay, cp_decay, cp_decay_delay, tau):
     """
     Credits to Damien Grauser. Source:
     https://github.com/GoldenCheetah/GoldenCheetah/blob/master/src/Metrics/ExtendedCriticalPower.cpp
@@ -46,23 +48,23 @@ def _ecp_predict_7_3(model_params, x):
     model = (
         (
             (
-                model_params['power_anaerobic_alactic']
+                power_anaerobic_alactic
             ) * (
                 (
-                    np.exp(model_params['power_anaerobic_decay'])
+                    np.exp(power_anaerobic_decay)
                 ) * (
-                    np.power(x, model_params['power_anaerobic_alactic'])
+                    np.power(x, power_anaerobic_alactic)
                 )
             )
         ) + (
             (
-                model_params['cp'] * (1 - np.exp(model_params['tau_delay'] * x))
+                cp * (1 - np.exp(tau_delay * x))
             ) * (
-                1 - np.exp(model_params['cp_delay'] * x)
+                1 - np.exp(cp_delay * x)
             ) * (
-                1 + model_params['cp_decay'] * np.exp(model_params['cp_decay_delay'] / x)
+                1 + cp_decay * np.exp(cp_decay_delay / x)
             ) * (
-                1 + model_params['tau'] / x
+                1 + tau / x
             )
         )
     )
@@ -74,34 +76,34 @@ def _ecp_predict_7_3(model_params, x):
     return model
 
 
-def _ecp_residual(model_params, x, data, version):
+def model_fit(max_efforts, time_axis, version='extended_5_3'):
     if version == 'extended_5_3':
-        model = _ecp_predict_5_3(model_params, x)
+        predict_func = extended_5_3_predict
+        initial_model_params = np.array([811, -2, 280, -0.9, -0.583, -180, 1.208, -4.8])
+        model_param_names = [
+            'power_anaerobic_alactic',
+            'power_anaerobic_decay',
+            'cp',
+            'cp_delay',
+            'cp_decay',
+            'cp_decay_delay',
+            'tau',
+            'tau_delay'
+        ]
     elif version == 'extended_7_3':
-        model = _ecp_predict_7_3(model_params, x)
-    return data - model
+        predict_func = extended_7_3_predict
+        initial_model_params = np.array([811, -2, 280, -0.9, -0.583, -180, 1.208, -4.8])
+        model_param_names = [
+            'power_anaerobic_alactic',
+            'power_anaerobic_decay',
+            'cp',
+            'cp_delay',
+            'cp_decay',
+            'cp_decay_delay',
+            'tau',
+            'tau_delay'
+        ]
 
+    model_params, _ = curve_fit(predict_func, time_axis, max_efforts, initial_model_params)
 
-def critical_power_model(mean_max_power, version='extended_5_3', initial_model_params=None):
-    if initial_model_params is None:
-        initial_model_params = dict()
-    mean_max_power = mean_max_power[:5400]
-
-    time_axis = np.asarray(range(1, len(mean_max_power) + 1))
-
-    # Initial model parameters
-    model_params = Parameters()
-    model_params.add_many(
-        ('power_anaerobic_alactic', initial_model_params.get('power_anaerobic_alactic', 811)),
-        ('power_anaerobic_decay', initial_model_params.get('power_anaerobic_decay', -2)),
-        ('cp', initial_model_params.get('cp', 280)),
-        ('cp_delay', initial_model_params.get('cp_delay', -0.9)),
-        ('cp_decay', initial_model_params.get('cp_decay', -0.583)),
-        ('cp_decay_delay', initial_model_params.get('cp_delay_decay', -180)),
-        ('tau', initial_model_params.get('tau', 1.208)),
-        ('tau_delay', initial_model_params.get('tau_delay', -4.8)),
-    )
-
-    model = minimize(_ecp_residual, model_params, args=(time_axis, mean_max_power, version))
-
-    return model
+    return dict(zip(model_param_names, model_params))
--- tests/algorithms/test_critical_power.py
+++ tests/algorithms/test_critical_power.py
@@ -5,19 +5,19 @@ import pytest
 from athletic_pandas.algorithms import critical_power, main
 
 
-@pytest.mark.parametrize('predict_func,version,model_params', [
+@pytest.mark.parametrize('predict_func,version,expected_model', [
     (
         critical_power._ecp_predict_5_3,
         'extended_5_3',
         dict(
-            power_anaerobic_alactic=-50069.215835935684,
-            power_anaerobic_decay=-56.252291794719689,
-            cp=461.10854374316568,
-            cp_delay=-0.95214799279593743,
-            cp_decay=-0.97239480096068875,
-            cp_decay_delay=-401.24531894589637,
-            tau=0.65413728252995684,
-            tau_delay=-23.775957048235664
+            power_anaerobic_alactic=811.0,
+            power_anaerobic_decay=-2.0,
+            cp=280.0,
+            cp_delay=-0.90000000000000002,
+            cp_decay=-0.58299999999999996,
+            cp_decay_delay=-180.0,
+            tau=1.208,
+            tau_delay=-4.7999999999999998
         )
     ),
     (
@@ -27,7 +27,7 @@ from athletic_pandas.algorithms import critical_power, main
             power_anaerobic_alactic=811,
             power_anaerobic_decay=-2.0,
             cp=280.0,
-            cp_delay=-1.0073579624642723,
+            cp_delay=-0.90000000000000002,
             cp_decay=-0.58299999999999996,
             cp_decay_delay=-180.0,
             tau=1.208,
@@ -35,9 +35,13 @@ from athletic_pandas.algorithms import critical_power, main
         )
     ),
 ])
-def test_critical_power(predict_func, version, model_params):
+def test_critical_power(predict_func, version, expected_model):
     power = pd.Series(range(500))
     mean_max_power = main.mean_max_power(power)
-    model = critical_power.critical_power_model(mean_max_power, version=version)
+    model = critical_power.critical_power_model(
+        mean_max_power,
+        np.linspace(1, 501, num=500),
+        version=version
+    )
     
-    assert model.params == model_params
+    assert model == expected_model
