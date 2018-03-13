6e356d896408a3258c5a8ea55b03aee2c84d140f
sladkovm <sladkovm@gmail.com>
2018-03-13 22:07:47 +0100

    mean_max

--- sweat/algorithms/metrics/core.py
+++ sweat/algorithms/metrics/core.py
@@ -260,3 +260,40 @@ def weighted_average_power(arg, mask=None, value=0.0, **kwargs):
     rv = np.mean(np.power(_rolling_mean, 4)) ** (1/4)
 
     return rv
+
+
+def mean_max(arg, mask=None, value=0.0, **kwargs):
+    """Mean-max curve
+
+    Compute mean-max (power duration curve) from the stream. Mask-filter options can be
+    added using the keyword arguments.
+
+    Parameters
+    ----------
+    arg : array-like
+        Power stream
+    mask: array-like, optional
+        Replacement mask (the default is None, which implies no masking)
+    value: number, optional
+        Value to use as a replacement (the default is 0.0)
+
+    Returns
+    -------
+    rv : type of input argument
+        Power-Duration Curve
+    """
+
+    y = mask_fill(arg, mask=mask, value=value)
+    y = pd.Series(y)
+
+    # Compute the accumulated energy from the power data
+    energy = y.cumsum()
+
+    # Compute the maximum sustainable power using the difference in energy
+    # This method is x4 faster than using rolling mean
+    y = np.array([])
+    for t in np.arange(1, len(energy)):
+        y = np.append(y, energy.diff(t).max()/(t))
+    y = cast_array_to_original_type(y, type(arg))
+
+    return y
--- tests/algorithms/metrics/test_core.py
+++ tests/algorithms/metrics/test_core.py
@@ -1,9 +1,10 @@
 import numpy as np
+import pandas as pd
 from unittest import mock
 from sweat.algorithms.metrics.core import (mask_fill, rolling_mean,
                                            median_filter, compute_zones,
                                            best_interval, time_in_zones,
-                                           weighted_average_power)
+                                           weighted_average_power, mean_max)
 
 
 class TestMaskFill():
@@ -246,5 +247,47 @@ class TestWeightedAveragePower():
         moving = np.ones(30, dtype=bool)
 
         assert weighted_average_power(stream, moving, type='xPower') == 1
+
+
+class TestMeanMax():
+
+    def test_power_duration_curve(self):
+        power = np.arange(101)
+        rv = mean_max(power)
+
+        assert type(power) == np.ndarray
+        assert rv[0] == 100.0
+        assert rv[49] == 75.5
+        assert rv[-1] == 50.5
+
+
+    def test_power_duration_curve_list(self):
+        stream = [0, 0, 0]
+        expected = [0, 0]
+
+        rv = mean_max(stream)
+
+        assert type(rv) == list
+        assert rv == expected
+
+
+    def test_power_duration_curve_ndarray(self):
+        stream = np.zeros(3)
+        expected = np.zeros(2)
+
+        rv = mean_max(stream)
+
+        assert type(rv) == np.ndarray
+        assert (rv == expected).all()
+
+
+    def test_power_duration_curve_series(self):
+        stream = pd.Series(np.zeros(3))
+        expected = pd.Series(np.zeros(2))
+
+        rv = mean_max(stream)
+
+        assert type(rv) == pd.Series
+        assert (rv == expected).all()
 
 
