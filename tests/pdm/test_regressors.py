0af66a9de215b82abb0d2cb91b3c98f606061905
Aart Goossens <aart@goossens.me>
2020-06-09 16:38:02 +0200

    Adds exponentional and omni power duration models

--- tests/pdm/test_regressors.py
+++ tests/pdm/test_regressors.py
@@ -8,31 +8,48 @@ from sweat.pdm import regressors
 
 @pytest.mark.skip()
 def test_top_level_import():
-    assert sweat.CriticalPowerRegressor == regressors.CriticalPowerRegressor
+    assert sweat.PowerDurationRegressor == regressors.PowerDurationRegressor
 
 
-class TestCriticalPowerRegressor:
+class TestPowerDurationRegressor:
     @pytest.mark.skip()
     def test_base(self):
-        check_estimator(sweat.CriticalPowerRegressor())
+        check_estimator(sweat.PowerDurationRegressor())
 
     def test_2_param(self):
-        cpreg = sweat.CriticalPowerRegressor()
+        cpreg = sweat.PowerDurationRegressor()
         cpreg.fit([[1.0], [60.0], [1200.0]], [1500.0, 600.0, 350.0])
         assert isinstance(cpreg.predict([[60], [61]]), np.ndarray)
         assert hasattr(cpreg, "cp_")
         assert hasattr(cpreg, "w_prime_")
 
-        cpreg = sweat.CriticalPowerRegressor(model="2 param")
+        cpreg = sweat.PowerDurationRegressor(model="2 param")
         cpreg.fit([[1.0], [60.0], [1200.0]], [1500.0, 600.0, 350.0])
         assert isinstance(cpreg.predict([[60], [61]]), np.ndarray)
         assert hasattr(cpreg, "cp_")
         assert hasattr(cpreg, "w_prime_")
 
     def test_3_param(self):
-        cpreg = sweat.CriticalPowerRegressor(model="3 param")
+        cpreg = sweat.PowerDurationRegressor(model="3 param")
         cpreg.fit([[1.0], [60.0], [1200.0]], [1500.0, 600.0, 350.0])
         assert isinstance(cpreg.predict([[60], [61]]), np.ndarray)
         assert hasattr(cpreg, "cp_")
         assert hasattr(cpreg, "w_prime_")
         assert hasattr(cpreg, "p_max_")
+
+    def test_exp(self):
+        cpreg = sweat.PowerDurationRegressor(model="exponential")
+        cpreg.fit([[1.0], [60.0], [1200.0]], [1500.0, 600.0, 350.0])
+        assert isinstance(cpreg.predict([[60], [61]]), np.ndarray)
+        assert hasattr(cpreg, "cp_")
+        assert hasattr(cpreg, "p_max_")
+        assert hasattr(cpreg, "tau")
+
+    def test_omni(self):
+        cpreg = sweat.PowerDurationRegressor(model="omni")
+        cpreg.fit([[1.0], [60.0], [1200.0], [3600.0]], [1500.0, 600.0, 350.0, 300.0])
+        assert isinstance(cpreg.predict([[60], [61]]), np.ndarray)
+        assert hasattr(cpreg, "cp_")
+        assert hasattr(cpreg, "p_max_")
+        assert hasattr(cpreg, "w_prime_")
+        assert hasattr(cpreg, "a_")
