8668735cc8fc3479120937e46fae78f4813df291
Aart Goossens <aart@goossens.me>
2021-04-20 19:11:34 +0200

    Fixes integer columns for read_fit()

--- sweat/io/fit.py
+++ sweat/io/fit.py
@@ -18,7 +18,7 @@ from .utils import (
 
 def process_location_columns(df, columns=None):
     if columns is None:
-        columns = [c for c in df.columns if c.endswith("_lat") or c.endswith("_long")]
+        columns = [c for c in df.columns if isinstance(c, str) and (c.endswith("_lat") or c.endswith("_long"))]
     else:
         columns = [c for c in columns if c in df.columns]
 
